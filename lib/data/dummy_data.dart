2378c10dffc53e012f2cc723d2c0d47635a49112
martirosianartur <martirosianartur4@gmail.com>
2024-07-26 17:30:26 +0200

    add and display category items on the screen

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import 'package:tolma/models/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c11',
+    title: 'Armenian',
+    color: Colors.deepPurple,
+  ),
+  Category(
+    id: 'c12',
+    title: 'Ukrainian',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c13',
+    title: 'Georgian',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c14',
+    title: 'Russian',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'c15',
+    title: 'Kazakh',
+    color: Colors.lime,
+  ),
+  Category(
+    id: 'c16',
+    title: 'Austrian',
+    color: Colors.yellow,
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tolma/categories.dart';
+import 'package:tolma/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,

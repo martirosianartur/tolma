408730c2aa8015df30d80f2c5d40e1780cb96bd1
martirosianartur <martirosianartur4@gmail.com>
2024-07-27 14:38:59 +0200

    make drawer items (Filters, Meals) tapable

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  final void Function(String identifier) onSelectScreen;
+
+  const MainDrawer({super.key, required this.onSelectScreen});
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +53,9 @@ class MainDrawer extends StatelessWidget {
               style: Theme.of(context).textTheme.titleSmall!.copyWith(
                   color: Theme.of(context).colorScheme.onSurface, fontSize: 24),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('meals');
+            },
           ),
           ListTile(
             leading: Icon(
@@ -64,7 +68,9 @@ class MainDrawer extends StatelessWidget {
               style: Theme.of(context).textTheme.titleSmall!.copyWith(
                   color: Theme.of(context).colorScheme.onSurface, fontSize: 24),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('filters');
+            },
           ),
         ],
       ),

7892f731d05fc16660867327a699cda3940458a6
martirosianartur <martirosianartur4@gmail.com>
2024-07-28 17:20:04 +0200

    replace depricated property

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -25,13 +25,13 @@ class FiltersScreen extends ConsumerWidget {
             title: Text(
               'Gluten-free',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             subtitle: Text(
               'Only include gluten-free meals.',
               style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             activeColor: Theme.of(context).colorScheme.tertiary,
@@ -47,13 +47,13 @@ class FiltersScreen extends ConsumerWidget {
             title: Text(
               'Lactose-free',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             subtitle: Text(
               'Only include lactose-free meals.',
               style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             activeColor: Theme.of(context).colorScheme.tertiary,
@@ -69,13 +69,13 @@ class FiltersScreen extends ConsumerWidget {
             title: Text(
               'Vegetarian',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             subtitle: Text(
               'Only include vegetarian meals.',
               style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             activeColor: Theme.of(context).colorScheme.tertiary,
@@ -91,13 +91,13 @@ class FiltersScreen extends ConsumerWidget {
             title: Text(
               'Vegan',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             subtitle: Text(
               'Only include vegan meals.',
               style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    color: Theme.of(context).colorScheme.onSurface,
                   ),
             ),
             activeColor: Theme.of(context).colorScheme.tertiary,
